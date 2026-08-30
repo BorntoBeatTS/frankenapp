39e62ee71d98302a30cb1256ffadbe64eb12fe8d
TΣ <rmontoya.leiva03@gmail.com>
2026-08-29 22:31:29 -0600

    Add artifacts/frankenapp/src/components/MutationLab/organImages.ts

---
+++ artifacts/frankenapp/src/components/MutationLab/organImages.ts
@@ -0,0 +1,23 @@
+import vaultEye     from '/organs/organ-vault-eye.png';
+import phoneWorm     from '/organs/organ-phone-worm.png';
+import brain         from '/organs/organ-brain.png';
+import mouth         from '/organs/organ-mouth.png';
+import lensCluster   from '/organs/organ-lens-cluster.png';
+import megaphoneEar  from '/organs/organ-megaphone-ear.png';
+import keypadBox     from '/organs/organ-keypad-box.png';
+import periscopeEye  from '/organs/organ-periscope-eye.png';
+import compassSpider from '/organs/organ-compass-spider.png';
+import handCamera    from '/organs/organ-hand-camera.png';
+import cameraLens    from '/organs/organ-camera-lens.png';
+import speaker       from '/organs/organ-speaker.png';
+import neuralNet     from '/organs/organ-neural-net.png';
+import dollarWheel   from '/organs/organ-dollar-wheel.png';
+import tentacle      from '/organs/organ-tentacle.png';
+import cannon        from '/organs/organ-cannon.png';
+
+export const organImages = [
+  vaultEye, phoneWorm, brain, mouth,
+  lensCluster, megaphoneEar, keypadBox, periscopeEye,
+  compassSpider, handCamera, cameraLens, speaker,
+  neuralNet, dollarWheel, tentacle, cannon,
+];
